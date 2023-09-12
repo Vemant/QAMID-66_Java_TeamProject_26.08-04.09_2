fdfa67c80904b2f9fdb2c648bbf88a8cd3f78b7d
Mikhail Verbitskii <vemant@yandex.ru>
2023-09-12 17:32:26 +0300

    Java credit 17_2

--- src/main/java/ru/netology/javaqadiplom/CreditAccount.java
+++ src/main/java/ru/netology/javaqadiplom/CreditAccount.java
@@ -12,11 +12,22 @@ public class CreditAccount extends Account {
      * Создаёт новый объект кредитного счёта с заданными параметрами.
      * Если параметры некорректны (кредитный лимит отрицательный и так далее), то
      * должно выкидываться исключения вида IllegalArgumentException.
+     *
      * @param initialBalance - неотрицательное число, начальный баланс для счёта
-     * @param creditLimit - неотрицательное число, максимальная сумма которую можно задолжать банку
-     * @param rate - неотрицательное число, ставка кредитования для расчёта долга за отрицательный баланс
+     * @param creditLimit    - неотрицательное число, максимальная сумма которую можно задолжать банку
+     * @param rate           - неотрицательное число, ставка кредитования для расчёта долга за отрицательный баланс
      */
     public CreditAccount(int initialBalance, int creditLimit, int rate) {
+        if (initialBalance <= -creditLimit) {
+            throw new IllegalArgumentException(
+                    "Начальный баланс должен быть выше кредитного лимита"
+            );
+        }
+        if (creditLimit <= 0) {
+            throw new IllegalArgumentException(
+                    "Абсолютное значение кредитного лимита должно быть положительным, а у вас: " + creditLimit
+            );
+        }
         if (rate <= 0) {
             throw new IllegalArgumentException(
                     "Накопительная ставка не может быть отрицательной, а у вас: " + rate
@@ -33,6 +44,7 @@ public class CreditAccount extends Account {
      * на сумму покупки. Если же операция может привести к некорректному
      * состоянию счёта (например, баланс может уйти меньше чем лимит), то операция должна
      * завершиться вернув false и ничего не поменяв на счёте.
+     *
      * @param amount - сумма покупки
      * @return true если операция прошла успешно, false иначе.
      */
@@ -41,9 +53,8 @@ public class CreditAccount extends Account {
         if (amount <= 0) {
             return false;
         }
-        balance = balance - amount;
-        if (balance > -creditLimit) {
-            balance = -amount;
+        if (amount <= creditLimit + balance) {
+            balance = balance - amount;
             return true;
         } else {
             return false;
@@ -56,9 +67,10 @@ public class CreditAccount extends Account {
      * на сумму покупки. Если же операция может привести к некорректному
      * состоянию счёта, то операция должна
      * завершиться вернув false и ничего не поменяв на счёте.
+     *
      * @param amount - сумма пополнения
-     * @return true если операция прошла успешно, false иначе.
      * @param amount
+     * @return true если операция прошла успешно, false иначе.
      * @return
      */
     @Override
@@ -66,24 +78,37 @@ public class CreditAccount extends Account {
         if (amount <= 0) {
             return false;
         }
-        balance = amount;
+        balance = balance + amount;
         return true;
     }
 
     /**
-     * Операция расчёта процентов на отрицательный баланс счёта при условии, что
-     * счёт не будет меняться год. Сумма процентов приводится к целому
-     * числу через отбрасывание дробной части (так и работает целочисленное деление).
-     * Пример: если на счёте -200 рублей, то при ставке 15% ответ должен быть -30.
-     * Пример 2: если на счёте 200 рублей, то при любой ставке ответ должен быть 0.
+     * Операция расчёта процентов на отрицательный
+     * баланс счёта при условии, что
+     * счёт не будет меняться год.
+     * Сумма процентов приводится к целому
+     * числу через отбрасывание дробной части
+     * (так и работает целочисленное деление).
+     * Пример: если на счёте -200 рублей,
+     * то при ставке 15% ответ должен быть -30.
+     * Пример 2: если на счёте 200 рублей,
+     * то при любой ставке ответ должен быть 0.
+     *
      * @return
      */
     @Override
     public int yearChange() {
-        return balance / 100 * rate;
+        if (balance < 0) {
+            return balance / 100 * rate;
+        } else {
+            return 0;
+        }
+
     }
 
     public int getCreditLimit() {
         return creditLimit;
     }
 }
+
+
